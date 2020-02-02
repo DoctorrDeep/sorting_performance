0348dbe7213192c142e42a157f78ffe5a86a5987
Ambar <ambar@calendar42.com>
2020-02-02 23:00:11 +0100

    Update Readme, generator for random numbers written

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# sorting_performance
-Try sorting algorithms and evaluate performance by time and number of steps
+
+## Why does this exist?
+
+- trying different sorting techniques to see difference in performance
+- understand (if possible) trade-off between heavy computations and number of computations
+(a-la would you fight 1 horse sized duck or 100 duck size horses)
+
+## How to run it?
+
+Please run in python 3 for best results. No special packages required.
+Some testing/coverage based tests will be written. Those might need a 
+look see in the `requirements.txt` file
+
+### Sorting performance
+Try sorting algorithms and evaluate performance by 
+- total time to solve
+- number of steps taken to solve
+
+### Inspiration
+
+- local supermarket is giving out free football player cards
+- each card has a number so that unique orders can be established
+- this helps in keeping track of what cards we have and which ones we want to trade
+- card numbers range from 1 to 250
+- arranging all cards in to packs is a daily chore
+  - **Pack 1** : unique cards (sorted)
+  - **Pack 2**: extra copies of some cards in pack 1
+
+Doing this on the dining table, 
+I realized that both me and my son are using various methods
+of sorting! Constantly trying new sorting methods to either
+- *speed up the process*
+- OR *slow it down and dumb it down so that we can do it while chatting or watching cartoons*
+
+## Credits
+
+- [reddit thread](https://www.reddit.com/r/learnpython/comments/exese6/what_are_some_of_the_projects_i_can_start_working/fg7skxp/)
+- [P vs NP problem explanation video in youtube uses a sorting based example](https://youtu.be/EHp4FPyajKQ?t=515)
---
+++ app_scripts/create_check_random_number_list.py
@@ -0,0 +1,67 @@
+import random
+
+def generate_list(min_number:int=1, max_number:int=1000000, count:int=1000, uniqued_list:bool=True) -> list:
+    """
+    This function will create a list of random numbers.
+    It accepts min number in list, max number in list and count of numbers in list.
+
+
+    :param min_number: mininum value of single number in list of random numbers
+    :param max_number: maximum value of single number in list of random numbers
+    :param count: number of random numbers expected
+    :param uniqued_list: boolean of whether or not the returned random list is allowed to have duplicate values or not
+    :return: list of size `count` of random numbers in random order
+    """
+
+    random_numbers_list = []
+
+    while not len(random_numbers_list) > count:
+
+        temp = random.randint(min_number, max_number)
+
+        if uniqued_list:
+            if temp in random_numbers_list:
+                continue
+
+        random_numbers_list.append(temp)
+
+    if not check_order(random_numbers_list)["random_bool"]:
+        # If the generated list is somehow ordered then run the generator
+        # until randomness found. Useful in test scenarios.
+        generate_list(min_number, max_number, count, uniqued_list)
+
+    return random_numbers_list
+
+
+def check_order(list_of_numbers: list)-> bool:
+    """
+    Take a list of numbers and returns whether the list
+    was ordered in ascending manner or not
+
+    :param list_of_numbers:
+    :return: dictionary with 1 key `random_bool`.
+        - Value True means the list is random.
+        - Value False means the list is ordered in an ascending manner.
+
+    Doctest
+
+    >>> check_order([1,2,3])
+    {'random_bool': False}
+
+    >>> check_order([2,2,3])
+    {'random_bool': False}
+
+    >>> check_order([3,2,3])
+    {'random_bool': True}
+
+    """
+
+    state_of_randomness = {"random_bool": False}
+
+    for index, num in enumerate(list_of_numbers[:-1]):
+        if num>list_of_numbers[index + 1]:
+            state_of_randomness["random_bool"] = True
+
+    return state_of_randomness
+
+
