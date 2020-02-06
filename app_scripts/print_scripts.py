575d967b8ed78affc7f22b685ebebd4cc646f9d9
Ambar <ambar@calendar42.com>
2020-02-06 02:10:54 +0100

    reformatting code for easy readability using black

--- app_scripts/print_scripts.py
+++ app_scripts/print_scripts.py
@@ -13,7 +13,7 @@ def print_sort_results(
         matches_known_solution: bool = None,
         help_text: str = "",
 ):
-    result_f_string = f"Sort {method_name} {help_text} took {time_taken_to_sort} seconds to order in {step_count} steps. Check: Sort status = {sort_state}."
+    result_f_string = f"{method_name} {help_text} took {time_taken_to_sort} seconds to order in {step_count} steps. Check: Sort status = {sort_state}."
 
     if matches_known_solution is not None:
         print(
